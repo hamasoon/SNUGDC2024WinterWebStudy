091ef875c7223088d775396f4a09281ad5dccd27
hamasoon@naver.com <hamasoon@naver.com>
2024-01-10 02:21:04 +0900

    first commit

---
+++ Assignment/Week1/jwp-basic-step2-user-with-mvc-framework/src/main/java/next/dao/DBConnector.java
@@ -0,0 +1,140 @@
+package next.dao;
+
+import core.jdbc.ConnectionManager;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+
+public class DBConnector {
+    private Logger log;
+
+    public DBConnector() {
+        log = LoggerFactory.getLogger("DBLogger");
+    }
+
+    private void dbClosing(PreparedStatement pstmt, Connection con) throws SQLException {
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    private void dbClosing(PreparedStatement pstmt, Connection con, ResultSet rs) throws SQLException {
+        try {
+            dbClosing(pstmt, con);
+
+            if(rs == null)
+                rs.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    private PreparedStatement sqlSetter(Connection con, String sql, String...var) throws SQLException {
+        PreparedStatement pstmt = null;
+
+        try {
+            pstmt = con.prepareStatement(sql);
+            for (int i = 0; i < var.length; i++)
+                pstmt.setString(i+1, var[i]);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return pstmt;
+    }
+
+    public void runNonQuery(String sql, String...var) throws Exception {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = sqlSetter(con, sql, var);
+
+            pstmt.executeUpdate();
+            dbClosing(pstmt, con);
+        } catch (Exception e) {
+            e.printStackTrace();
+            dbClosing(pstmt, con);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> runQuery(String sql, Class<T> resultTypeClass, String...var) throws Exception {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        ResultSetMetaData rsm = null;
+        List<T> ret = new ArrayList<T>();
+
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = sqlSetter(con, sql, var);
+
+            rs = pstmt.executeQuery();
+            rsm = rs.getMetaData();
+            int colCount = rsm.getColumnCount();
+
+            if (resultTypeClass != null) {
+                while (rs.next()) {
+                    T instance = null;
+
+                    try {
+                        instance = resultTypeClass.newInstance();
+                    } catch (InstantiationException | IllegalAccessException e) {
+                        e.printStackTrace();
+                        dbClosing(pstmt, con, rs);
+                        throw new RuntimeException(e);
+                    }
+
+                    if(instance != null) {
+                        Field[] Fields = resultTypeClass.getDeclaredFields();
+                        for (int i = 1; i <= colCount; i++) {
+                            String columnLabel = rsm.getColumnLabel(i);
+                            for (Field field : Fields) {
+                                if (field.getName().toLowerCase().equals(columnLabel.toLowerCase())) {
+                                    field.setAccessible(true);
+                                    try {
+                                        field.set(instance, rs.getObject(i));
+                                        System.out.println("객체의 " + field.getName() + "에 값(" + rs.getObject(i) + ")을 적용함");
+                                    } catch (IllegalArgumentException | IllegalAccessException e) {
+                                        e.printStackTrace();
+                                        dbClosing(pstmt, con, rs);
+                                        throw new RuntimeException(e);
+                                    }
+                                }
+                            }
+                        }
+                        ret.add(instance);
+                    }
+                }
+            }
+
+            dbClosing(pstmt, con, rs);
+
+            return ret;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            dbClosing(pstmt, con, rs);
+
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Assignment/Week1/jwp-basic-step2-user-with-mvc-framework/src/main/java/next/dao/UserDao.java
@@ -0,0 +1,67 @@
+package next.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import core.jdbc.ConnectionManager;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserDao {
+    private DBConnector connector;
+    private Logger log;
+
+    public UserDao() {
+        connector = new DBConnector();
+
+        log = LoggerFactory.getLogger("DaoLogger");
+    }
+
+    public void insert(User user) throws SQLException {
+        try {
+            connector.runNonQuery("INSERT INTO USERS VALUES (?, ?, ?, ?)",
+                    user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        } catch (Exception e) {
+            log.error("INSERT ERROR", e);
+        }
+    }
+
+    public void update(User user) throws SQLException {
+        try {
+            connector.runNonQuery("UPDATE USERS SET (userId, password, name, email)=(?, ?, ?, ?) WHERE userId=?",
+                    user.getUserId(), user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+        } catch (Exception e) {
+            log.error("UPDATE ERROR", e);
+        }
+    }
+
+    public List<User> findAll() throws SQLException {
+        List<User> result = new ArrayList<>();
+
+        try {
+            result = connector.<User>runQuery("SELECT * FROM USERS", User.class);
+
+        } catch (Exception e) {
+            log.error("UPDATE ERROR", e);
+        }
+
+        return result;
+    }
+
+    public User findByUserId(String userId) throws SQLException {
+        User ret = null;
+
+        try {
+            ret = connector.<User>runQuery("SELECT userId, password, name, email FROM USERS WHERE userid=?", User.class, userId).get(0);
+
+        } catch (Exception e) {
+            log.error("FIND ERROR", e);
+        }
+        return ret;
+    }
+}
